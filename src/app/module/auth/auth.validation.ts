4709b6be5a1d94e70b8b6f95421222140042516c
Masad Rayan <masadrayan2002@gmail.com>
2026-08-15 15:17:41 +0600

    feat(auth): enhance patient registration with validation schema and contact number field

---
+++ src/app/module/auth/auth.validation.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+export const PatientRegistrationZodSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: z.email("Invalid email address"),
+  password: z
+    .string()
+    .min(5, "Password must be at least 5 characters long")
+    .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+    .regex(/[0-9]/, "Password must contain at least one number")
+    .regex(
+      /[^A-Za-z0-9]/,
+      "Password must contain at least one special character",
+    ),
+  patient: z
+    .object({
+      contactNumber: z.string().optional(),
+    })
+    .optional(),
+});
