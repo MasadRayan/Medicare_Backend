400908d1c1de98a1a75578da17caf484cf89b8d4
Masad Rayan <masadrayan2002@gmail.com>
2026-08-21 18:14:56 +0600

    feat(test): add test route to verify Redis client functionality

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, {
+    type NextFunction,
 	type Application,
 	type Request,
 	type Response,
@@ -10,6 +11,7 @@ import config from "./app/config";
 import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
 import { notFound } from "./app/middleware/notFound";
 import { AuthRoutes } from "./app/module/auth/auth.route";
+import { redisClient } from "./app/lib/redis";
 
 const app: Application = express();
 
@@ -28,6 +30,31 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", AuthRoutes);
+
+app.get("/test", async (req: Request, res: Response, next: NextFunction) => {
+	try {
+
+		await redisClient.set("forget-password-otp:user@gmail.com", "123456", {
+			expiration: {
+				type: "EX",
+				value: 300,
+			}
+		});
+		
+
+
+		res.status(httpStatus.OK).json({
+		success: true,
+		message: "Test route is working",
+	});
+	} catch (error) {
+		res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+		success: false,
+		message: "Test route is not working",
+		data: error,
+	});
+	}
+});
 
 // Basic route
 app.get("/", async (req: Request, res: Response) => {
