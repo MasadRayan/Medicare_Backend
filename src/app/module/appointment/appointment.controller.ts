2f12b8bd0f32056d8774e8bae039b1df241b30df
Masad Rayan <masadrayan2002@gmail.com>
2026-09-03 18:06:33 +0600

    feat(appointment): enhance payment handling in bookAppointment and bookAppointmentPaymentCallback; add payment status updates and redirect URLs

--- src/app/module/appointment/appointment.controller.ts
+++ src/app/module/appointment/appointment.controller.ts
@@ -5,35 +5,25 @@ import httpStatus from "http-status";
 import { AppointmentService } from "./appointment.service";
 
 const bookAppointment = catchAsync(async (req: Request, res: Response) => {
-	const payload = req.body;
-	const user = req.user;
-	const result = await AppointmentService.bookAppointment(payload, user!);
-	sendResponse(res, {
-		statusCode: httpStatus.CREATED,
-		success: true,
-		message: "Appointment booked successfully",
-		data: result,
-	});
+  const payload = req.body;
+  const user = req.user;
+  const result = await AppointmentService.bookAppointment(payload, user!);
+  sendResponse(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: "Appointment booked successfully",
+    data: result,
+  });
 });
 
 const bookAppointmentPaymentCallback = catchAsync(
-	async (req: Request, res: Response) => {
-		const { bkashExecutePaymentResult, redirectURL } =
-			await AppointmentService.bookAppointmentPaymentCallback(req.query);
-		console.log(bkashExecutePaymentResult);
-
-		res.redirect(redirectURL);
-
-		// sendResponse(res, {
-		//   statusCode: httpStatus.OK,
-		//   success: true,
-		//   message: "Payment callback handled successfully",
-		//   data: result,
-		// });
-	},
+  async (req: Request, res: Response) => {
+    const { redirectURL } = await AppointmentService.bookAppointmentPaymentCallback(req.query);
+    res.redirect(redirectURL);
+  },
 );
 
 export const AppointmentController = {
-	bookAppointment,
-	bookAppointmentPaymentCallback,
+  bookAppointment,
+  bookAppointmentPaymentCallback,
 };
