dcfb1d66e55f84c4ffb82bed38470ab8b13da7b3
Masad Rayan <masadrayan2002@gmail.com>
2026-09-11 02:35:12 +0600

    feat(appointment): implement getDoctorAppointments with pagination, sorting, and error handling

--- src/app/module/appointment/appointment.service.ts
+++ src/app/module/appointment/appointment.service.ts
@@ -774,7 +774,59 @@ const getMyAppointments = async (query: IQuery, user: RequestUser) => {
   };
 };
 
-const getDoctorAppointments = async () => {};
+const getDoctorAppointments = async (query: IQuery, user: RequestUser) => {
+	const limit = query.limit ? Number(query.limit) : 10;
+	const page = query.page ? Number(query.page) : 1;
+	const skip = (page - 1) * limit;
+	const sortBy = query.sortBy ? query.sortBy : "createdAt";
+	const sortOrder = query.sortOrder ? query.sortOrder : "desc"
+
+	const doctor = await prisma.doctor.findUnique({
+		where: { userId: user.userId },
+	});
+
+	if (!doctor) {
+		throw new AppError(httpStatus.NOT_FOUND, "Doctor Profile Not Found");
+	}
+
+	const andConditions: AppointmentWhereInput[] = [
+		{
+			doctorId : doctor.id
+		}
+	];
+
+	if (query.status) {
+		andConditions.push({ status: query.status });
+	}
+
+	const appointments = await prisma.appointment.findMany({
+		where: { AND: andConditions },
+		take: limit,
+		skip,
+		orderBy: { [sortBy] : sortOrder},
+		include: {
+			patient: {
+				select: { id: true, name: true, email: true, contactNumber: true },
+			},
+			schedule: true,
+			payment: true,
+		},
+	});
+
+	const total = await prisma.appointment.count({
+		where: { AND: andConditions },
+	});
+
+	return {
+		data: appointments,
+		meta: {
+			page,
+			limit,
+			total,
+			totalPages: Math.ceil(total / limit),
+		},
+	};
+};
 
 const getAllAppointments = async () => {};
 
