98fe15ef126d206314ad52ac153d8f33326b6bb1
Masad Rayan <masadrayan2002@gmail.com>
2026-09-10 20:14:11 +0600

    feat(corn): enhance deleteUnverifiedDoctors function with error handling and logging

--- src/app/lib/corn.ts
+++ src/app/lib/corn.ts
@@ -1,7 +1,32 @@
 import cron from "node-cron";
+import { prisma } from "./prisma";
+import { DoctorverificationStatus, Role } from "../../generated/prisma/enums";
 
 export const deleteUnverifiedDoctors = async () => {
-	cron.schedule("*/2 * * * * *", () => {
-		console.log("Delete Doctor");
+	cron.schedule("*/2 * * * * *", async () => {
+		try {
+            const oneHourAgo = new Date(Date.now() - 60 * 60 * 1000);
+            const deleteDoctor = await prisma.user.deleteMany({
+                where: {
+                    role: Role.DOCTOR,
+                    emailVerified: false,
+                    createdAt: {
+                        lt: oneHourAgo
+                    },
+                    doctor: {
+                        verificationStatus: DoctorverificationStatus.PENDING
+                    }
+                }
+            })
+
+            if (deleteDoctor.count > 0) {
+                console.log(`Cron: Deleted ${deleteDoctor.count} unverified doctor applications`);
+            }
+
+        } catch (error) {
+            console.log("Cron: Failed to delete unverified doctor applications", error);
+        }
 	});
+
+    console.log("Unverified Doctor Delete cron schedule (every 10 minutes)")
 };
