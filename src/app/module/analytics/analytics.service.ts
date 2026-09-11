59b84561637351bab5772481ada4f4ff99f43139
Masad Rayan <masadrayan2002@gmail.com>
2026-09-11 19:03:00 +0600

    feat(analytics): implement admin analytics with comprehensive doctor and appointment statistics

--- src/app/module/analytics/analytics.service.ts
+++ src/app/module/analytics/analytics.service.ts
@@ -1,5 +1,64 @@
+import { AppointmentStatus, DoctorverificationStatus } from "../../../generated/prisma/browser";
+import { prisma } from "../../lib/prisma";
 
 const getAdminAnalytics = async () => {
+    const totalDoctors = await prisma.doctor.count({
+        where : {
+            isDeleted: false
+        }
+    });
+    const totalPendingDoctorApplication = await prisma.doctor.count({
+        where : {
+            isDeleted: false,
+            verificationStatus: DoctorverificationStatus.PENDING
+        }
+    });
+    const totalapprovedDoctors = await prisma.doctor.count({
+        where : {
+            isDeleted: false,
+            verificationStatus: DoctorverificationStatus.APPROVED
+        }
+    });
+    const totalRejectedDoctors = await prisma.doctor.count({
+        where : {
+            isDeleted: false,
+            verificationStatus: DoctorverificationStatus.REJECTED
+        }
+    });
+    const totalPatients = await prisma.patient.count({
+        where : {
+            isDeleted: false
+        }
+    });
+    const totalAppointments = await prisma.appointment.count();
+    const totalcompletedAppointments = await prisma.appointment.count({
+        where : {
+            status: AppointmentStatus.COMPLETED
+        }
+    });
+    const totalcancelledAppointments = await prisma.appointment.count({
+        where : {
+            status: AppointmentStatus.CANCELLED
+        }
+    });
+    const totalpendingAppointments = await prisma.appointment.count({
+        where : {
+            status: AppointmentStatus.PENDING
+        }
+    });
+
+
+    return {
+        totalDoctors,
+        totalPendingDoctorApplication,
+        totalapprovedDoctors,
+        totalRejectedDoctors,
+        totalPatients,
+        totalAppointments,
+        totalcompletedAppointments,
+        totalcancelledAppointments,
+        totalpendingAppointments
+    }
 
 }
 
